7a8ac3dd32aafb71df9c064f85c1e277d14cc885
jal333222111-svg <jal333222111@gmail.com>
2025-09-22 16:02:37 +0700

    tambahkan  dan hapus

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,14 +1,16 @@
-import AppLogoIcon from './app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
-            </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
+            {/* Langsung tampilkan logo tanpa background */}
+            <img
+                src="/favicon.ico"
+                alt="Logo Job Management"
+                className="h-10 w-auto"
+            />
+
+            <div className="ml-2 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+                    Job Management
                 </span>
             </div>
         </>
